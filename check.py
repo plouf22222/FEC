7c59ee3d6975ee21346467da29d633a1ab085156
plouf22222 <plouf.2222@laposte.net>
2016-09-10 12:12:52 +0200

    minor change
    new :
    output to a file
    list of file by header

--- check.py
+++ check.py
@@ -2,18 +2,20 @@ import os
 
 header = dict()
 
-# L = [os.path.join(os.getcwd(),f) for f in os.listdir('.') if os.path.isfile(os.path.join(os.getcwd(),f))]
-L = [ "test.txt.byte"]
+L = [os.path.join(os.getcwd(),f) for f in os.listdir('.') if os.path.isfile(os.path.join(os.getcwd(),f))]
 for fichier in L:
-    if fichier[-3:] != '.py':
+    if fichier[-3:] != 'txt':
         f_in=open(fichier, 'r')
         data = f_in.readline()
         if data in header:
-            header[data] += 1
+            header[data][0] += 1
+            header[data].append(fichier)
         else:
-            header[data] = 1
+            header[data] = [1]
+            header[data].append(fichier)
         f_in.close()
-
+f_out = open("headers.csv",'w')
 for key in header:
-    print(header[key], " Header => ", key)
+    f_out.write(header[key])
+    f_out.write(key)
 
